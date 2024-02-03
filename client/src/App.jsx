7fd154c40974267011d6ed85d7f7fe6745f06586
AhmedReda958 <areda.gad@gmail.com>
2024-02-03 23:18:42 +0200

    setup fontawesome

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
 function App() {
-  return <div className="text-5xl text-primary">hello</div>;
+  return (
+    <div className="text-5xl text-primary">
+      hello <i class="fa-solid fa-bell"></i>
+    </div>
+  );
 }
 
 export default App;
