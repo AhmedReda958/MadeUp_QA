6d2ea39915be8a0b09337ecf7975eced65065986
AhmedReda958 <areda.gad@gmail.com>
2024-02-03 17:20:27 +0200

    get user data by username

--- server/api/routes/userRoute.js
+++ server/api/routes/userRoute.js
@@ -1,10 +1,26 @@
 // routes/user.js
+
+import connectToDB from "../../utils/database.js";
+import User from "../../models/user.js";
+
 import express from "express";
 const router = express.Router();
 
-router.get("/:id", (req, res) => {
-  const userId = req.params.id;
-  res.send(`User ID: ${userId}`);
+router.get("/:username", async (req, res) => {
+  const { username } = req.params;
+  try {
+    await connectToDB();
+    // Find the user by email or username
+    let user = await User.findOne({ username: username });
+
+    if (!user) {
+      return res.status(401).send("User not Found!");
+    }
+    return res.json(user);
+  } catch (error) {
+    console.error(error);
+    return new res.status(500).send("Internal Server Error");
+  }
 });
 
 export default router;
