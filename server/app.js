03e911c602ec595f6897b3a95bdbcbf3cb9c0b4c
AlMahllawi <mohammadalmahllawi@gmail.com>
2024-02-08 23:00:29 +0200

    Move app related options to app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,30 @@
 const { HOST, PORT } = process.env;
 import express from "express";
+import cors from "cors";
 let app; export default app = express();
+
+app.use(express.json());
+
+// app.use(cors({
+//   origin: function(origin, callback){
+//    // allow requests with no origin
+//    // (like mobile apps or curl requests)
+//    if(!origin) return callback(null, true);
+//    if(allowedOrigins.indexOf(origin) === -1){
+//      var msg = 'The CORS policy for this site does not ' +
+//                'allow access from the specified Origin.';
+//      return callback(new Error(msg), false);
+//    }
+//    return callback(null, true);
+//  }
+
+// }));
+// const corsOptions = {
+//   origin: process.env.CLIENT_URL || "http://localhost:5173",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+app.use(cors());
 
 app.listen(PORT, HOST, () => {
   console.log(`Listening on http://${HOST == "0.0.0.0" ? "localhost" : HOST}:${PORT}/`);
--- server/index.js
+++ server/index.js
@@ -1,44 +1,16 @@
 import "./env.js";
-import express from "express";
 import app from "./app.js";
 import api from "./api/index.js";
-import cors from "cors";
 
-// Database connection
 import { attemptDBConnection } from "./database/connection.js";
 attemptDBConnection();
 
-// Middleware to parse JSON requests
-app.use(express.json());
-
-// app.use(cors({
-//   origin: function(origin, callback){
-//    // allow requests with no origin
-//    // (like mobile apps or curl requests)
-//    if(!origin) return callback(null, true);
-//    if(allowedOrigins.indexOf(origin) === -1){
-//      var msg = 'The CORS policy for this site does not ' +
-//                'allow access from the specified Origin.';
-//      return callback(new Error(msg), false);
-//    }
-//    return callback(null, true);
-//  }
-
-// }));
-const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors());
-
 app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+  res.send("Backend is alive!");
 });
 
 app.use("/api", api);
 
-// 404 route - handle unknown routes
 app.use((req, res) => {
   res.status(404).send("404 - Not Found");
 });
