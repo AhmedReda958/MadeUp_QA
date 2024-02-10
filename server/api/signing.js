bd0b1ec2c4fbbdbb0d59dc471342b75e2fea87fa
AlMahllawi <mohammadalmahllawi@gmail.com>
2024-02-10 15:07:14 +0200

    update status

--- server/api/signing.js
+++ server/api/signing.js
@@ -20,7 +20,7 @@ router.post("/login", async (req, res) => {
     }, { email: 1, username: 1, password: 1 });
 
     if (!(user && await user.comparePassword(password)))
-    return res.status(user ? 403 : 401).json({ message: "Invalid email/username or password." });
+    return res.status(401).json({ found: !!user, message: "Invalid email/username or password." });
     
     const token = jwt.sign(
       { userId: user._id }, JWT_SECRET_KEY, { expiresIn: SIGNING_EXPIRY }
@@ -53,7 +53,7 @@ router.post("/register", async (req, res) => {
       { userId: user._id }, JWT_SECRET_KEY, { expiresIn: SIGNING_EXPIRY }
     );
 
-    return res.status(200).json({ 
+    return res.status(201).json({ 
       user: {
         _id: user._id,
         username: user.username,
