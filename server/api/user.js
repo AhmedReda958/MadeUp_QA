8c0d83993999b0cbf9cbd09abac17374a8c7dbf1
AlMahllawi <mohammadalmahllawi@gmail.com>
2024-02-10 23:19:43 +0200

    Modify API user route

--- server/api/user.js
+++ server/api/user.js
@@ -1,60 +1,31 @@
-// routes/user.js
 import User from "#database/models/user.js";
+import authMiddleware, { requiredAuthMiddleware } from "../middlewares/authorization.js";
 
 import express from "express";
-import authMiddleware from "../middlewares/authorization.js";
 const router = express.Router();
 
-// TODO: handle POST for user creation (register)
-
-// get user data
 router.get("/:username", authMiddleware,
 async (req, res, next) => { try {
   const { username } = req.params;
-  // Find the user by email or username
   let user = await User.findOne({ username: username });
   if (!user) return res.status(404).json({ error: "User not found" });
 
-  // ? not completed
-  // todo: - hide user private data
-  // if user logedin
-  if (req.userId) {
-    // his own profile
-    if (req.userId === user._id.valueOf()) {
-      return res.json(user);
-    }
-
-    return res.json(user);
-  } else {
-    // TODO: hide private info
-    return res.json(user);
-  }
+  // if (req.userId == user._id) // TODO: hide private info
+  return res.status(200).json(user);
 } catch (err) { next(err); }});
 
-// TODO: Change PUT to be PATCH
-// Update user profile route
-router.put("/update/:user_id", authMiddleware,
+router.patch("/", authMiddleware, requiredAuthMiddleware,
 async (req, res, next) => { try {
-  const updatedProfile = req.body; // Assuming you send the updated profile details in the request body
+  const updatedProfile = req.body; // TODO: validate
 
-  // Find the user by ID
-  const user = await User.findById(req.params.user_id);
+  const user = await User.findById(req.userId);
+  if (!user) return res.status(409).json({ error: "Conflict." }); // TODO: handle, the user is logged in but not saved in the database
 
-  if (!user) {
-    return res.status(404).json({ error: "User not found" });
-  }
+  if (req.userId != user._id) return res.status(403).json({ error: "Forbidden." });
+  Object.assign(user, updatedProfile);
 
-  // if user logedin && profile owner
-  if (req.userId && req.userId === user._id.valueOf()) {
-    // Update user profile fields
-    Object.assign(user, updatedProfile);
-
-    // Save the updated user
-    await user.save();
-    return res.json({ message: "User profile updated successfully", user });
-  } else {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
+  await user.save();
+  return res.status(200).json(user);
 } catch (err) { next(err); }});
 
 export default router;
