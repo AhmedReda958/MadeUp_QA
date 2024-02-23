01ed35d06da64b08b5b0bcc90c604620a5f58182
AlMahllawi <mohammadalmahllawi@gmail.com>
2024-02-23 03:59:41 +0200

    Modify pagination values validation

--- server/middlewares/pagination.mjs
+++ server/middlewares/pagination.mjs
@@ -1,11 +1,25 @@
+import IntegerRange from "#lib/range.mjs";
+let inputs = {
+  page: {
+    range: new IntegerRange(1, Infinity, 1, true, false),
+    default: 1
+  },
+  limit:  {
+    range: new IntegerRange(1, 100),
+    default: 50
+  }
+}
+
 export default function paginationMiddleware(req, res, next) {
-  let inputs = ["page", "limit"], issues = {};
+  let issues = {};
   req.pagination = {};
-  for(let input of inputs) {
-    let value = input in req.query ? Number(req.query[input]) : 1;
-    if (Number.isInteger(value) && value > 0)
-      req.pagination[input] = value;
-    else issues[input] = "Not a positive integer.";
+  for(let inputName in inputs) {
+    let input = inputs[inputName];
+    let value = inputName in req.query ? Number(req.query[inputName]) : input.default;
+    if (!input.range.includes(value)) 
+      req.pagination[inputName] = value;
+    else
+      issues[inputName] = "Not in range.";
   };
   if (Object.keys(issues).length > 0)
     return res.status(400).json({ code: "INVALID", issues });
