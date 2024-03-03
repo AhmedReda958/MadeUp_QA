dd0ff6aa3b8c50fd7e69b3bd40867bf3840f774f
AlMahllawi <mohammadalmahllawi@gmail.com>
2024-03-03 20:39:41 +0200

    handle jwt verification

--- server/middlewares/authorization.mjs
+++ server/middlewares/authorization.mjs
@@ -1,13 +1,23 @@
+import { CommonError } from "#errors/index.mjs";
 const { JWT_SECRET_KEY } = process.env;
 import jwt from "jsonwebtoken";
 
 export default function authMiddleware(req, res, next) {
   const token = req.headers.authorization?.replace("Bearer ", "");
+  console.log(token)
   if (!token) return next();
+  console.log(token)
 
-  const decodedToken = jwt.verify(token, JWT_SECRET_KEY);
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, JWT_SECRET_KEY);
+  } catch (err) {
+    // TODO: log errors + handle
+    console.error(new CommonError(err, "Unable to verify the jwt."));
+  }
+
   // TODO: check if changed password after token iat and cancel auth
-  req.userId = decodedToken.userId;
+  req.userId = decodedToken?.userId;
 
   next();
 }
