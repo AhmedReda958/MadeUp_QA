5363f655500824927a2a47a7f1153674911a51d0
AhmedReda958 <areda.gad@gmail.com>
2024-03-04 12:46:28 +0200

    connect to production data base

--- server/database/connection.mjs
+++ server/database/connection.mjs
@@ -1,5 +1,5 @@
 import DatabaseError from "#errors/database.mjs";
-const { MONGODB_URI } = process.env;
+const { MONGODB_URI, NODE_ENV } = process.env;
 import mongoose from "mongoose";
 
 export function isDBConnected() {
@@ -9,10 +9,15 @@ export function isDBConnected() {
 export function attemptDBConnection() {
   mongoose.set("strictQuery");
   if (isDBConnected()) return console.log("Already connected to the database.");
-  mongoose.connect(MONGODB_URI, { dbName: "anon_app" }).then(() => {
-    console.error("Connected to the database!");
-  }).catch(error => {
-    // TODO: log errors
-    console.error(new DatabaseError("CONNECT", error));
-  });
+  mongoose
+    .connect(MONGODB_URI, {
+      dbName: NODE_ENV === "production" ? "MadeUp" : "anon_app",
+    })
+    .then(() => {
+      console.error("Connected to the database!");
+    })
+    .catch((error) => {
+      // TODO: log errors
+      console.error(new DatabaseError("CONNECT", error));
+    });
 }
