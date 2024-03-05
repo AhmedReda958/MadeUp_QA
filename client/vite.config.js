df1a049873bae2c25bebe66dd5adb9e4457471ae
AhmedReda958 <areda.gad@gmail.com>
2024-03-05 20:05:57 +0200

    change themecolor

--- client/vite.config.js
+++ client/vite.config.js
@@ -37,7 +37,7 @@ const manifest = {
       purpose: "maskable",
     },
   ],
-  theme_color: "#3b82f6",
+  theme_color: "#000007",
   id: "/",
   description: "Send and anwser Anonymous messages",
   dir: "auto",
