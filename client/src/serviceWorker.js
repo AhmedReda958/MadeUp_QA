5810963282f094873d3c288b9ed2fa240f57e95d
AhmedReda958 <areda.gad@gmail.com>
2024-03-10 15:59:33 +0200

    notifications/subscribe from service worker | not tested yet

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -1,4 +1,6 @@
 import { precacheAndRoute, cleanupOutdatedCaches } from "workbox-precaching";
+import { urlBase64ToUint8Array } from "./utils/helpers";
+import axios from "axios";
 
 // Register precache routes (static cache)
 precacheAndRoute(self.__WB_MANIFEST);
@@ -9,6 +11,26 @@ cleanupOutdatedCaches();
 // Install and activate service worker
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", () => self.clients.claim());
+
+const publicVapidKey = "your-public-vapid-key"; // REPLACE_WITH_YOUR_KEY
+
+// Register SW, Register Push, Send Push
+async function send() {
+  console.log("Registering Push...");
+  const subscription = await self.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
+  });
+  console.log("Push Registered...");
+
+  // Send Push Notification
+  console.log("Sending Push...");
+  await axios.post("notifications/subscribe", subscription);
+  و;
+
+  console.log("Push Sent...");
+}
+send().catch((err) => console.error(err));
 
 // Receive push notifications
 self.addEventListener("push", function (e) {
