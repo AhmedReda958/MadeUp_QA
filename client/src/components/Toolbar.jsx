a523e9a518124fdf3d2f03c85eddc38ecd04e972
AhmedReda958 <areda.gad@gmail.com>
2024-03-10 06:29:12 +0200

    fake notifiction count

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -12,8 +12,8 @@ const Toolbar = () => {
   let username = userInfo.username;
   const links = [
     { to: "/", icon: "fa-compass" },
-    { to: "/notification", icon: "fa-bell" },
-    { to: "/messages", icon: "fa-envelope" },
+    { to: "/notification", icon: "fa-bell", count: !logedin ? 2 : null },
+    { to: "/messages", icon: "fa-envelope", count: !logedin ? 3 : null },
     { to: `/${username}`, icon: "fa-user" },
   ];
 
@@ -22,8 +22,8 @@ const Toolbar = () => {
   }, [logedin]);
 
   return (
-    <div className=" fixed z-30 bottom-0 left-0 w-full pt-4 px-6 pb-5 drop-shadow-4xl ">
-      <div className=" relative bg-dark dark:bg-gray-300 text-white dark:text-dark  max-w-96 m-auto h-14 text-xl rounded-t-[20px] rounded-b-[30px] flex items-center justify-around px-4">
+    <div className=" fixed z-30 bottom-0 left-0 w-full pt-4 px-6 pb-5 ">
+      <div className=" relative bg-dark  dark:bg-gray-300/70 text-white dark:text-dark backdrop-blur-lg max-w-96 m-auto h-14 text-xl rounded-t-[20px] rounded-b-[30px] flex items-center justify-around px-4">
         {links.map((link, index) => (
           <NavLink
             key={index}
@@ -32,7 +32,15 @@ const Toolbar = () => {
               isPending ? pendingClasses : isActive ? activeClasses : ""
             }
           >
-            <i className={`${iconClasses} ${link.icon}`}></i>
+            <div className=" relative">
+              {link.count && (
+                <span className=" absolute -top-1 -right-2 rounded-full bg-primary w-4 h-4 text-xs text-white text-center">
+                  {link.count}
+                </span>
+              )}
+
+              <i className={`${iconClasses} ${link.icon}`}></i>
+            </div>
           </NavLink>
         ))}
       </div>
