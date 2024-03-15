99e2a629a696cdaba3a1837998f26bf1b6c4b481
AhmedReda958 <areda.gad@gmail.com>
2024-03-15 02:27:49 +0200

    mark notifications and messages as unseen | api endpoint

--- server/api/app.mjs
+++ server/api/app.mjs
@@ -6,6 +6,7 @@ import authMiddleware, {
 import express from "express";
 const router = express.Router();
 
+// get unseen notifications count
 router.get(
   "/unseen",
   authMiddleware,
@@ -21,6 +22,49 @@ router.get(
         seen: false,
       });
       res.status(200).json({ notificationsCount, messagesCount });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// mark notifications and messages as unseen
+router.patch(
+  "/mark-as-seen",
+  authMiddleware,
+  requiredAuthMiddleware,
+  async (req, res, next) => {
+    try {
+      const { type } = req.query;
+      const markNotifications = async () =>
+        await notification.updateMany(
+          {
+            user: req.userId,
+            seen: false,
+          },
+          { seen: true }
+        );
+
+      const markMessages = async () =>
+        await message.updateMany(
+          {
+            receiver: req.userId,
+            seen: false,
+          },
+          { seen: true }
+        );
+
+      if (type === "notificatoins") {
+        await markNotifications();
+        res.json({ message: "All unseen notifications marked as seen" });
+      } else if (type === "messages") {
+        await markMessages();
+        res.json({ message: "All unseen messages marked as seen" });
+      } else {
+        await markNotifications();
+        await markMessages();
+        res.json({ message: "All unseen marked as seen" });
+      }
     } catch (error) {
       next(error);
     }
