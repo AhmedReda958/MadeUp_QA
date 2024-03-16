ccbf6884803fc331600a4036839a918fe1836211
AhmedReda958 <areda.gad@gmail.com>
2024-03-16 23:43:06 +0200

    install oneSignalSDK

--- client/src/utils/oneSignal.js
+++ client/src/utils/oneSignal.js
@@ -5,6 +5,7 @@ export default async function runOneSignal() {
     appId: import.meta.env.VITE_ONESIGNAL_APP_ID,
     safari_web_id: import.meta.env.VITE_ONESIGNAL_SAFARI_WEB_ID,
     allowLocalhostAsSecureOrigin: true,
+    serviceWorkerPath: "/serviceWorker",
   });
   OneSignal.Slidedown.promptPush();
 }
