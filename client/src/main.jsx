1db6129322dc171b5a93ef687fc6c56cc0b3da5b
AhmedReda958 <areda.gad@gmail.com>
2024-03-20 04:32:24 +0200

    Update manifest.json

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -22,8 +22,6 @@ import "@fontsource-variable/rubik";
 //only 400
 import "@fontsource-variable/cairo-play";
 import "@fontsource/rubik-doodle-shadow";
-import registerSW from "./service_workers/registerSW";
-import runOneSignal from "@/utils/oneSignal";
 
 // axios config
 const apiURL = import.meta.env.VITE_API_URL || "http://localhost:8000/api/";
