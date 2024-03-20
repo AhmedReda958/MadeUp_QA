153fb34ac95575fadacf9135929989bec44c0593
AlMahllawi <mohammadalmahllawi@gmail.com>
2024-03-21 01:26:58 +0200

    comment template

--- server/tools/events.mjs
+++ server/tools/events.mjs
@@ -4,12 +4,12 @@ const events = new EventEmitter();
 export default events;
 
 import OneSignal, { OneSignalAPI, ONESIGNAL_APP_ID } from "./one-signal.mjs";
-const RECEIVED_MESSAGE_TEMPLATE_ID = "364498a2-74d7-43da-8f88-6bd4e3116576"; // TODO: configure
+// const RECEIVED_MESSAGE_TEMPLATE_ID = "158ce485-bd11-4ee4-8063-5c812d1402ff"; // TODO: configure
 events.on("MessageSent", (message) => {
   // TODO
   let notification = new OneSignal.Notification();
   notification.app_id = ONESIGNAL_APP_ID;
-  notification.template_id = RECEIVED_MESSAGE_TEMPLATE_ID;
+  // notification.template_id = RECEIVED_MESSAGE_TEMPLATE_ID;
   notification.contents = { en: message.content };
   notification.include_external_user_ids = [message.receiver];
   // {
