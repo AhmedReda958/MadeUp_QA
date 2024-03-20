331e89d27240dba277aacedc8d7fcfaed6f90796
AhmedReda958 <areda.gad@gmail.com>
2024-03-20 23:26:03 +0200

    Refactor toolbar and header components

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -1,13 +1,17 @@
 import { NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getNotificationsCount } from "@/redux/actions/notificationsActions";
+import { Transition } from "@headlessui/react";
 
 const iconClasses = "far";
 const activeClasses = "text-primary";
 const pendingClasses = "pending";
 
 const Toolbar = () => {
+  // make transition for the toolbar
+  const [show, setShow] = useState(true);
+
   const { userInfo, logedin } = useSelector((state) => state.auth);
   const { unseen } = useSelector((state) => state.app);
   const dispatch = useDispatch();
@@ -40,29 +44,64 @@ const Toolbar = () => {
     { to: `/${username}`, icon: "fa-user" },
   ];
 
-  return (
-    <div className=" fixed z-30 bottom-0 left-0 w-full pt-4 px-6 pb-5 ">
-      <div className=" relative bg-dark  dark:bg-gray-300/70 text-white dark:text-dark backdrop-blur-lg max-w-96 m-auto h-14 text-xl rounded-t-[20px] rounded-b-[30px] flex items-center justify-around px-4">
-        {links.map((link, index) => (
-          <NavLink
-            key={index}
-            to={logedin || link.to === "/" ? link.to : "/login"}
-            className={({ isActive, isPending }) =>
-              isPending ? pendingClasses : isActive ? activeClasses : ""
-            }
-          >
-            <div className=" relative">
-              {link.count > 0 && (
-                <span className=" absolute -top-1 -right-2 rounded-full bg-primary w-4 h-4 text-xs text-white text-center">
-                  {link.count}
-                </span>
-              )}
+  useEffect(() => {
+    const main = document.querySelector("main");
 
-              <i className={`${iconClasses} ${link.icon}`}></i>
-            </div>
-          </NavLink>
-        ))}
-      </div>
+    const onScroll = () => {
+      let lastScroll = 0;
+      const toolbar = document.querySelector(".toolbar");
+      main.addEventListener("scroll", () => {
+        let currentScroll = main.scrollTop;
+
+        if (currentScroll > lastScroll) {
+          setShow(false);
+        } else {
+          setShow(true);
+        }
+        lastScroll = currentScroll;
+      });
+    };
+
+    onScroll();
+
+    return () => {
+      main.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
+  return (
+    <div className="fixed z-30 bottom-0 left-0 w-full pt-4 px-6 pb-5 ">
+      <Transition
+        show={show}
+        enter="ease duration-[.3s]"
+        enterFrom=" translate-y-64"
+        enterTo=" translate-y-0"
+        leave="ease duration-[1s]"
+        leaveFrom=" translate-y-0"
+        leaveTo="  translate-y-64"
+      >
+        <div className=" relative bg-dark  dark:bg-gray-300/70 text-white dark:text-dark backdrop-blur-lg max-w-96 m-auto h-14 text-xl rounded-t-[20px] rounded-b-[30px] flex items-center justify-around px-4">
+          {links.map((link, index) => (
+            <NavLink
+              key={index}
+              to={logedin || link.to === "/" ? link.to : "/login"}
+              className={({ isActive, isPending }) =>
+                isPending ? pendingClasses : isActive ? activeClasses : ""
+              }
+            >
+              <div className=" relative">
+                {link.count > 0 && (
+                  <span className=" absolute -top-1 -right-2 rounded-full bg-primary w-4 h-4 text-xs text-white text-center">
+                    {link.count}
+                  </span>
+                )}
+
+                <i className={`${iconClasses} ${link.icon}`}></i>
+              </div>
+            </NavLink>
+          ))}
+        </div>
+      </Transition>
     </div>
   );
 };
--- client/src/components/ui/Page.jsx
+++ client/src/components/ui/Page.jsx
@@ -4,29 +4,82 @@ import { useState, useEffect, Fragment } from "react";
 import { Transition } from "@headlessui/react";
 
 const Header = ({ title, children }) => {
-  return (
-    <div className="flex justify-between items-center h-12 py-2 mb-3 ">
-      <div className="flex items-center ">
-        <h1 className="font-bold text-2xl font-logo text-black dark:text-white ps-1">
-          MadeUp
-        </h1>
-        <h2 className="ms-5 border-s-2 border-body-alt ps-3 text-lg ">
-          {title}
-        </h2>
-      </div>
-      {/* back link */}
+  const [isSticky, setIsSticky] = useState(false);
+  const [isShown, setIsShown] = useState(false);
 
-      {children ? (
-        children
-      ) : (
-        <Link
-          to={-1}
-          className="pt-1 pe-3 cursor-pointer transition-all hover:text-primary duration-100 ease-in-out"
+  useEffect(() => {
+    const main = document.querySelector("#main-app");
+
+    const onScroll = () => {
+      let lastScroll = 0;
+      main.addEventListener("scroll", () => {
+        let currentScroll = main.scrollTop;
+
+        if (currentScroll > lastScroll) {
+          setIsSticky(false);
+          setIsShown(false);
+        } else if (currentScroll < 450) {
+          setIsSticky(false);
+        } else {
+          setIsSticky(true);
+          setIsShown(true);
+        }
+        lastScroll = currentScroll;
+      });
+    };
+
+    onScroll();
+
+    return () => {
+      main.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
+  return (
+    <Transition
+      show={isShown}
+      as={Fragment}
+      enter="ease duration-[.4s]"
+      enterFrom=" -translate-y-64"
+      enterTo=" translate-y-0"
+      leave="ease duration-[.4s]"
+      leaveFrom="-translate-y-64 "
+      leaveTo="translate-y-0  "
+    >
+      <div
+        className={` contianer w-full z-20 bg-maincolor mb-3 ${
+          isSticky && "fixed top-0 left-0 py-0 mb-0"
+        }`}
+      >
+        <div
+          className={`flex justify-between items-center h-12 ${
+            isSticky &&
+            "m-auto  w-full h-16 px-5 max-w-[768px] drop-shadow-sm bg-maincolor"
+          }`}
         >
-          <i className="fa-solid fa-arrow-left"></i>
-        </Link>
-      )}
-    </div>
+          <div className="flex items-center">
+            <h1 className="font-bold text-2xl font-logo text-black dark:text-white ps-1">
+              MadeUp
+            </h1>
+            <h2 className="ms-5 border-s-2 border-body-alt ps-3 text-lg">
+              {title}
+            </h2>
+          </div>
+          {/* back link */}
+
+          {children ? (
+            children
+          ) : (
+            <Link
+              to={-1}
+              className="pt-1 pe-3 cursor-pointer transition-all hover:text-primary duration-100 ease-in-out"
+            >
+              <i className="fa-solid fa-arrow-left"></i>
+            </Link>
+          )}
+        </div>
+      </div>
+    </Transition>
   );
 };
 
@@ -38,7 +91,7 @@ const Page = (props) => {
   useEffect(() => setAnimate(true), []);
 
   return (
-    <div className={`pb-20 ${className}`}>
+    <div className={`${className}`}>
       {header && <Header title={title} />}
       <Transition
         as={"div"}
@@ -47,7 +100,7 @@ const Page = (props) => {
         enterFrom="opacity-0 "
         enterTo="opacity-100 "
       >
-        {!loading ? children : <LoadingSpinner />}
+        <div className="px-30">{!loading ? children : <LoadingSpinner />}</div>
       </Transition>
     </div>
   );
