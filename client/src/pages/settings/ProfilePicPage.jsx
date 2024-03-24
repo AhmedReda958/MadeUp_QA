5e618d9ae2ffff2da50acd1e1c23a8b8de4e181b
AhmedReda958 <areda.gad@gmail.com>
2024-03-24 02:14:08 +0200

    image editor ui

--- client/src/pages/settings/ProfilePicPage.jsx
+++ client/src/pages/settings/ProfilePicPage.jsx
@@ -3,6 +3,7 @@ import { Button } from "flowbite-react";
 import axios from "axios";
 import Page from "@/components/ui/Page";
 import ProfilePic from "@/components/ProfilePic";
+import AvatarEditor from "react-avatar-editor";
 import LoadingSpinner from "@/components/ui/LoadingSpinner";
 import { CameraIcon } from "@heroicons/react/24/solid";
 import useAlert from "@/utils/hooks/useAlert";
@@ -14,7 +15,9 @@ const ProfilePicPage = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [showEditor, setShowEditor] = useState(false);
   const fileInput = useRef(null);
+
   const userInfo = useSelector((state) => state.auth.userInfo);
 
   const Alert = useAlert();
@@ -23,6 +26,7 @@ const ProfilePicPage = () => {
   const handleImageSelect = (event) => {
     const file = event.target.files[0];
     setSelectedImage(file);
+    setShowEditor(true);
   };
 
   const handleImageUpload = async (event) => {
@@ -74,15 +78,20 @@ const ProfilePicPage = () => {
     <Page title="Profile Picture">
       <form onSubmit={handleImageUpload}>
         <div className="flex justify-center">
-          <ProfilePic
-            imgUrl={
-              selectedImage
-                ? URL.createObjectURL(selectedImage)
-                : userInfo.profilePicture
-            }
-            className="w-40 h-40 shadow "
-          />
+          {!showEditor ? (
+            <ProfilePic
+              imgUrl={
+                selectedImage
+                  ? URL.createObjectURL(selectedImage)
+                  : userInfo.profilePicture
+              }
+              className="w-40 h-40 shadow "
+            />
+          ) : (
+            <ImageEditor image={selectedImage} />
+          )}
         </div>
+
         {loading && <LoadingSpinner className="mt-4" />}
         <Button
           as="label"
@@ -92,16 +101,16 @@ const ProfilePicPage = () => {
         >
           <CameraIcon className="w-6 h-6 me-3" />
           Choose new picture
+          <input
+            type="file"
+            accept="image/*"
+            id="imgUploader"
+            ref={fileInput}
+            onChange={handleImageSelect}
+            disabled={loading}
+            hidden
+          />
         </Button>
-        <input
-          type="file"
-          accept="image/*"
-          id="imgUploader"
-          ref={fileInput}
-          onChange={handleImageSelect}
-          disabled={loading}
-          hidden
-        />
         {error && <p className="test-sm text-red-600">{error}</p>}
         <Button
           color="primary"
@@ -113,6 +122,45 @@ const ProfilePicPage = () => {
         </Button>
       </form>
     </Page>
+  );
+};
+
+const ImageEditor = ({ image }) => {
+  const [scale, setScale] = useState(1);
+
+  const editorRef = useRef(null);
+
+  const handleScale = (event) => {
+    setScale(parseFloat(event.target.value));
+  };
+  return (
+    <div>
+      <AvatarEditor
+        ref={editorRef}
+        image={image}
+        width={250}
+        height={250}
+        border={30}
+        color={[0, 0, 0, 0.6]} // RGBA
+        scale={scale}
+        rotate={0}
+        borderRadius={200}
+        className="border-4 border-dashed border-body-alt dark:border-body "
+      />
+      <div className="flex items-center gap-3 mt-5">
+        <input
+          name="scale"
+          type="range"
+          onChange={handleScale}
+          min={1}
+          max="2"
+          step="0.01"
+          className="w-full "
+          defaultValue="1"
+        />
+        <span>{scale.toFixed(2)}x</span>
+      </div>
+    </div>
   );
 };
 
