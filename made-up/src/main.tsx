e64cb8a75d1126bc753677b35103ad0b8ee22503
AhmedReda958 <areda.gad@gmail.com>
2024-03-27 15:56:55 +0200

    install fonts

--- made-up/src/main.tsx
+++ made-up/src/main.tsx
@@ -6,6 +6,15 @@ import store from "@/redux/index.js";
 import { Flowbite } from "flowbite-react";
 import customTheme from "./theme/flowbiteThemeConfig";
 
+// fonts:
+// Supports weights 200-900
+import "@fontsource-variable/cairo";
+// Supports weights 300-900
+import "@fontsource-variable/rubik";
+//only 400
+import "@fontsource-variable/cairo-play";
+import "@fontsource/rubik-doodle-shadow";
+
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
