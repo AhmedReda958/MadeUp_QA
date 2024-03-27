ec87dc3d98b70c3e8a3f22d96a3b64332a4d689b
AhmedReda958 <areda.gad@gmail.com>
2024-03-27 17:40:01 +0200

    setup basic toolbar

--- made-up/src/pages/MainApp.tsx
+++ made-up/src/pages/MainApp.tsx
@@ -1,11 +1,17 @@
 import { useSelector, useDispatch } from "react-redux";
 import Toolbar from "@/components/Toolbar";
 import { useGetUserDetailsQuery } from "@/redux/services/authServices";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setCredentials } from "@/redux/slices/authSlice";
 import { checkInternetConnection } from "@/utils/handleConnection";
 import ShareDialog from "@/components/ui/ShareDialog";
-import { IonPage, IonRouterOutlet } from "@ionic/react";
+import {
+  IonPage,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+} from "@ionic/react";
 import { Route } from "react-router";
 
 // pages
@@ -16,8 +22,10 @@ import UserProfile from "@/pages/ProfilePage";
 import ReplayMessagePage from "@/pages/messages/ReplayMessagePage";
 import SettingsPage from "@/pages/settings/SettingsPage";
 import MessagesPage from "@/pages/messages/MessagesPage";
+import { IonReactRouter } from "@ionic/react-router";
 
 function MainApp() {
+  const [selectedTab, setSelectedTab] = useState("home");
   const app = useSelector((state) => state.app);
   const { userToken, logedin } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -51,14 +59,70 @@ function MainApp() {
           id="main-app"
           className="relative container w-full h-screen no-scrollbar max-w-[768px] mx-auto p-4 px-5 md-7 overflow-y-auto scroll-smooth"
         >
-          <IonRouterOutlet>
-            <Route path="/" exact={true} component={HomePage} />
-            <Route path="/notifications" component={NotificationPage} />
-            <Route path="/messages" component={MessagesPage} />
-            <Route path=":username" component={UserProfile} />
-          </IonRouterOutlet>
+          <IonReactRouter>
+            <IonTabs>
+              <IonRouterOutlet>
+                <Route path="/" render={() => <HomePage />} exact={true} />
+                <Route
+                  path="/notifications"
+                  render={() => <NotificationPage />}
+                  exact={true}
+                />
+                <Route
+                  path="/messages"
+                  render={() => <MessagesPage />}
+                  exact={true}
+                />
+                <Route
+                  path=":username"
+                  render={() => <UserProfile />}
+                  exact={true}
+                />
+              </IonRouterOutlet>
+              {/* toolbar */}
+              <IonTabBar
+                slot="bottom"
+                mode="ios"
+                // color={app.isDarkTheme ? "light" : "dark"}
+                translucent={true}
+                selectedTab={selectedTab}
+              >
+                <IonTabButton
+                  tab="home"
+                  href="/"
+                  selected={selectedTab === "home"}
+                  onClick={() => setSelectedTab("home")}
+                >
+                  home
+                </IonTabButton>
+                <IonTabButton
+                  tab="notifications"
+                  href="/notifications"
+                  selected={selectedTab === "notifications"}
+                  onClick={() => setSelectedTab("notifications")}
+                >
+                  notifications
+                </IonTabButton>
+                <IonTabButton
+                  tab="messages"
+                  href="/messages"
+                  selected={selectedTab === "messages"}
+                  onClick={() => setSelectedTab("messages")}
+                >
+                  messages
+                </IonTabButton>
+                <IonTabButton
+                  tab="profile"
+                  href="/profile"
+                  selected={selectedTab === "profile"}
+                  onClick={() => setSelectedTab("profile")}
+                >
+                  profile
+                </IonTabButton>
+              </IonTabBar>
+            </IonTabs>
+          </IonReactRouter>
         </main>
-        <Toolbar />
         <ShareDialog />
       </div>
     </IonPage>
