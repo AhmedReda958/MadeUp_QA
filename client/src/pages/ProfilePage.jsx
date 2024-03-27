05a1cec17ce3dd859743d7218383fcbab0840d2f
AhmedReda958 <areda.gad@gmail.com>
2024-03-27 03:39:38 +0200

    update share profile link text

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -265,6 +265,7 @@ const UserProfile = () => {
                   onClick={() =>
                     dispatch(
                       share({
+                        text: "رسل لي رسالة سرية دون أن أعرف من انت",
                         url: window.location.origin + "/" + userInfo.username,
                       })
                     )
