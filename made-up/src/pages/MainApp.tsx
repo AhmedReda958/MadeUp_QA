e49fc9bd1f217d8b239f55458e82dc85eedde701
AhmedReda958 <areda.gad@gmail.com>
2024-03-28 17:22:10 +0200

    tab bar

--- made-up/src/pages/MainApp.tsx
+++ made-up/src/pages/MainApp.tsx
@@ -1,16 +1,29 @@
 import { useSelector, useDispatch } from "react-redux";
 import Toolbar from "@/components/Toolbar";
 import { useGetUserDetailsQuery } from "@/redux/services/authServices";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setCredentials } from "@/redux/slices/authSlice";
 import { checkInternetConnection } from "@/utils/handleConnection";
 import ShareDialog from "@/components/ui/ShareDialog";
-import { IonPage, IonRouterOutlet } from "@ionic/react";
-import { Route } from "react-router";
+import {
+  IonPage,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+} from "@ionic/react";
+import { Redirect, Route } from "react-router";
+
+// icons
+import {
+  HomeIcon,
+  BellIcon,
+  EnvelopeIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 
 // pages
 import HomePage from "@/pages/HomePage";
-
 import NotificationPage from "@/pages/NotificationPage";
 import UserProfile from "@/pages/ProfilePage";
 import ReplayMessagePage from "@/pages/messages/ReplayMessagePage";
@@ -23,6 +36,7 @@ import ProfilePicPage from "@/pages/settings/ProfilePicPage";
 import PersonalInfoSettingsPage from "@/pages/settings/PersonalInfoSettingsPage";
 
 function MainApp() {
+  const [selectedTab, setSelectedTab] = useState("home");
   const app = useSelector((state) => state.app);
   const { userToken, logedin } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -56,27 +70,55 @@ function MainApp() {
           id="main-app"
           className="relative container w-full h-screen no-scrollbar max-w-[768px] mx-auto p-4 px-5 md-7 overflow-y-auto scroll-smooth"
         >
-          <IonRouterOutlet>
-            <Route path="/" exact={true} component={HomePage} />
-            <Route path="/user/:username" exact component={UserProfile} />
-            <Route path="/notifications" exact component={NotificationPage} />
-            <Route path="/messages" exact component={MessagesPage} />
-            <Route path="/messages/replay/:id" component={ReplayMessagePage} />
-            <Route path="/settings" exact component={SettingsPage} />
+          <IonTabs>
+            <IonRouterOutlet>
+              <Redirect exact path="/" to="/home" />
+              <Route path="/home" exact render={() => <HomePage />} />
+              <Route
+                path="/user/:username"
+                exact
+                render={() => <UserProfile />}
+              />
+              <Route
+                path="/notifications"
+                exact
+                render={() => <NotificationPage />}
+              />
+              <Route path="/messages" exact render={() => <MessagesPage />} />
 
-            <Route path="/message/:id" component={ShowMessagePage} />
-            <Route
-              path="/settings/profilePic"
-              component={ProfilePicPage}
-              exact
-            />
-            <Route
-              path="/settings/info"
-              component={PersonalInfoSettingsPage}
-              exact
-            />
-          </IonRouterOutlet>
-          <Toolbar />
+              <Route
+                path="/messages/replay/:id"
+                component={ReplayMessagePage}
+              />
+              <Route path="/settings" exact component={SettingsPage} />
+
+              <Route path="/message/:id" component={ShowMessagePage} />
+              <Route
+                path="/settings/profilePic"
+                component={ProfilePicPage}
+                exact
+              />
+              <Route
+                path="/settings/info"
+                component={PersonalInfoSettingsPage}
+                exact
+              />
+            </IonRouterOutlet>
+            <IonTabBar slot="bottom" translucent>
+              <IonTabButton tab="home" href="/home">
+                <HomeIcon className="w-6 h-6" />
+              </IonTabButton>
+              <IonTabButton tab="notifications" href="/notifications">
+                <BellIcon className="w-6 h-6" />
+              </IonTabButton>
+              <IonTabButton tab="messages" href="/messages">
+                <EnvelopeIcon className="w-6 h-6" />
+              </IonTabButton>
+              <IonTabButton tab="profile" href="/user/:username">
+                <UserIcon className="w-6 h-6" />
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
         </main>
         <ShareDialog />
       </div>
