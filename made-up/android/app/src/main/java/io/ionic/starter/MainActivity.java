ea05e123e70b6b20ec287cf07696feeae5f6316b
AhmedReda958 <areda.gad@gmail.com>
2024-03-30 01:10:54 +0200

    add icons and splash screens

--- made-up/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ made-up/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.madeup.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- made-up/capacitor.config.ts
+++ made-up/capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'MadeUp',
-  webDir: 'dist',
+  appId: "com.madeup.app",
+  appName: "MadeUp",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
 };
 
 export default config;
