534f8ebf023c5e9be156e96522cb1c2b39ea99b8
AhmedReda958 <areda.gad@gmail.com>
2024-04-04 17:26:32 +0200

    move mobile app to a new repo

--- made-up/android/app/src/main/java/com/madeup/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.madeup.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- made-up/src/pages/settings/GenralSettingsPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GenralSettingsPage = () => {
-  return <div>GenralSettingsPage</div>;
-};
-
-export default GenralSettingsPage;
